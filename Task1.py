65b1b1eee93755168df5e47a2d9e70f2d97d2c73
Niravgandhi21 <gandhinirav08@gmail.com>
2025-10-29 13:51:03 +0530

    Add assignment 1 tsks and README.

---
+++ Task1.py
@@ -0,0 +1,16 @@
+#Perform basic maths operations
+
+num1 = int(input("Enter the first nummber: "))
+num2 = int(input("Enter the second number: "))
+
+add = num1 + num2
+print("\nAddition: ",add)
+
+sub = num1 - num2
+print("Subtraction: ",sub)
+
+multiply = num1 * num2
+print("Multiplication: ",multiply)
+
+div = num1 / num2
+print("Division: ",div)
---
+++ Task2.py
@@ -0,0 +1,6 @@
+#create personalized greeting
+
+name = input("Enter your first name: ")
+surname = input("Enter your last name: ")
+
+print(f"Hello, {name} {surname}! Welcome to the python program. ")
